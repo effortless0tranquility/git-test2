091a8f886b63dbbcd083b27dd9cbd06671adc2a5
Willam <2022410106@qq.com>
2024-02-22 22:37:33 +0800

    master test

--- src/main/java/gitGroup/Main.java
+++ src/main/java/gitGroup/Main.java
@@ -1,11 +1,15 @@
 package gitGroup;
 
+/**
+ * @author William
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("Hello github!");
         System.out.println("Hello ThirdTest");
         System.out.println("Hello 4Test");
+        System.out.println("master test");
 
     }
 }
