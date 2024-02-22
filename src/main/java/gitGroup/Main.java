acdfc9615308c07407de481a5bf645e67e62ced7
Willam <2022410106@qq.com>
2024-02-22 22:03:57 +0800

    Second commit

---
+++ src/main/java/gitGroup/Main.java
@@ -0,0 +1,9 @@
+package gitGroup;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+        System.out.println("Hello github!");
+        System.out.println("Hello Java!");
+    }
+}
