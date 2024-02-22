642405b205cf124a41c4d26c6031e347b25dfd2a
Willam <2022410106@qq.com>
2024-02-22 22:28:23 +0800

    hot-fix commit 1

--- src/main/java/gitGroup/Main.java
+++ src/main/java/gitGroup/Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("Hello git!");
         System.out.println("Hello github!");
         System.out.println("Hello ThirdTest");
+        System.out.println("Hello 4Test");
+
     }
 }
